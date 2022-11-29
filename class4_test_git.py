3c04096ee78b05a5c49cbef68c650333db8a045f
Nusrat-96 <nusratadiba88@gmail.com>
2022-11-29 22:52:52 +0600

    first commit

--- class4_test_git.py
+++ class4_test_git.py
@@ -1,1 +1,8 @@
-print ("Hello")
+list1=[]
+for i in range (0,10):
+    input_list= (input())
+    if input_list in (list1):
+        print ("Already in the list")
+    else:
+        list1.append(input_list)
+print(list1)
